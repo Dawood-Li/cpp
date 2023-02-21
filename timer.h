8cb8d0b586877b1ff568b9ea4cc7d55e7249f577
Dawood Li <60247863+Dawood-Li@users.noreply.github.com>
2023-02-21 20:17:37 +0800

    Update timer.h

--- timer.h
+++ timer.h
@@ -6,20 +6,22 @@
 class Timer {
 
 public:
-	Timer() : Timer("timer") {}
 
-	Timer(std::string name) : 
-		_name(name),
-		_time_start(std::chrono::high_resolution_clock::now()) {}
+    Timer() : Timer("timer") {}
 
-	~Timer() {
-		_time_end = std::chrono::high_resolution_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(_time_end - _time_start);
-		std::cout << _name << " takes " << duration.count() / 1000000.0 << " ms\n";
-	}
+    Timer(std::string name) : 
+        _name(name),
+        _time_start(std::chrono::high_resolution_clock::now()) {}
+
+    ~Timer() {
+        _time_end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(_time_end - _time_start);
+        std::cout << _name << " takes " << duration.count() / 1000000.0 << " ms\n";
+    }
 
 private:
-	std::string _name;//先声明的先初始化，不会因为在_time_start后初始化导致不必要的性能惩罚
-	std::chrono::time_point<std::chrono::high_resolution_clock> _time_start;
-	std::chrono::time_point<std::chrono::high_resolution_clock> _time_end;
+
+    std::string _name;
+    std::chrono::time_point<std::chrono::high_resolution_clock> _time_start;
+    std::chrono::time_point<std::chrono::high_resolution_clock> _time_end;
 };
