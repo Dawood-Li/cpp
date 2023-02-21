83bff2b5dbe53d174c67d907c991ce39783725e7
Dawood Li <60247863+Dawood-Li@users.noreply.github.com>
2023-02-21 20:23:23 +0800

    Update timer.h

--- timer.h
+++ timer.h
@@ -9,19 +9,18 @@ public:
 
     Timer() : Timer("timer") {}
 
-    Timer(std::string name) : 
-        _name(name),
-        _time_start(std::chrono::high_resolution_clock::now()) {}
+    Timer(std::string name)
+        : _name(name)
+        , _start(std::chrono::high_resolution_clock::now()) {}
 
     ~Timer() {
-        _time_end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(_time_end - _time_start);
+        _end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(_end - _start);
         std::cout << _name << " takes " << duration.count() / 1000000.0 << " ms\n";
     }
 
 private:
 
     std::string _name;
-    std::chrono::time_point<std::chrono::high_resolution_clock> _time_start;
-    std::chrono::time_point<std::chrono::high_resolution_clock> _time_end;
+    std::chrono::time_point<std::chrono::high_resolution_clock> _start, _end;
 };
